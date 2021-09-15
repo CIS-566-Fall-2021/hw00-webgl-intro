6e37dfeb28d9620af7f25f19a263f9904fc3e333
Sam Lee <smlee18@sas.upenn.edu>
2021-09-15 18:05:54 -0400

    fixed ambient term

--- src/shaders/custom-vert.glsl
+++ src/shaders/custom-vert.glsl
@@ -35,102 +35,6 @@ out vec4 fs_Pos;
 const vec4 lightPos = vec4(5, 5, 3, 1); //The position of our virtual light, which is used to compute the shading of
                                         //the geometry in the fragment shader.
 
-/**
- * Linearly Re-maps a value from one range to another
- */
-float map(float value, float old_lo, float old_hi, float new_lo, float new_hi)
-{
-	float old_range = old_hi - old_lo;
-    if (old_range == 0.0) {
-	    return new_lo; 
-	} else {
-	    float new_range = new_hi - new_lo;  
-	    return (((value - old_lo) * new_range) / old_range) + new_lo;
-	}
-}
-
-/**
- * The canonical GLSL hash function
- */
-float hash(float x)
-{
-	return fract(sin(x) * 43758.5453123);
-}
-
-/** 
- * Nothing is mathematically sound about anything below: 
- * I just chose values based on experimentation and some 
- * intuitions I have about what makes a good hash function
- */
-vec3 gradient(vec3 cell)
-{
-	float h_i = hash(cell.x);
-	float h_j = hash(cell.y + pow(h_i, 3.0));
-	float h_k = hash(cell.z + pow(h_j, 5.0));
-    float ii = map(fract(h_i + h_j + h_k), 0.0, 1.0, -1.0, 1.0);
-    float jj = map(fract(h_j + h_k), 0.0, 1.0, -1.0, 1.0);
-	float kk = map(h_k, 0.0, 1.0, -1.0, 1.0);
-    return normalize(vec3(ii, jj, kk));
-}
-
-/**
- * Perlin's "ease-curve" fade function
- */
-float fade(float t)
-{
-   	float t3 = t * t * t;
-    float t4 = t3 * t;
-    float t5 = t4 * t;
-    return (6.0 * t5) - (15.0 * t4) + (10.0 * t3);        
-}    
-
-float noise(in vec3 coord)
-{
-    vec3 cell = floor(coord);
-    vec3 unit = fract(coord);
-   
-    vec3 unit_000 = unit;
-    vec3 unit_100 = unit - vec3(1.0, 0.0, 0.0);
-    vec3 unit_001 = unit - vec3(0.0, 0.0, 1.0);
-    vec3 unit_101 = unit - vec3(1.0, 0.0, 1.0);
-    vec3 unit_010 = unit - vec3(0.0, 1.0, 0.0);
-    vec3 unit_110 = unit - vec3(1.0, 1.0, 0.0);
-    vec3 unit_011 = unit - vec3(0.0, 1.0, 1.0);
-    vec3 unit_111 = unit - 1.0;
-
-    vec3 c_000 = cell;
-    vec3 c_100 = cell + vec3(1.0, 0.0, 0.0);
-    vec3 c_001 = cell + vec3(0.0, 0.0, 1.0);
-    vec3 c_101 = cell + vec3(1.0, 0.0, 1.0);
-    vec3 c_010 = cell + vec3(0.0, 1.0, 0.0);
-    vec3 c_110 = cell + vec3(1.0, 1.0, 0.0);
-    vec3 c_011 = cell + vec3(0.0, 1.0, 1.0);
-    vec3 c_111 = cell + 1.0;
-
-    float wx = fade(unit.x);
-    float wy = fade(unit.y);
-    float wz = fade(unit.z);
- 
-    float x000 = dot(gradient(c_000), unit_000);
-	float x100 = dot(gradient(c_100), unit_100);
-	float x001 = dot(gradient(c_001), unit_001);
-	float x101 = dot(gradient(c_101), unit_101);
-	float x010 = dot(gradient(c_010), unit_010);
-	float x110 = dot(gradient(c_110), unit_110);
-	float x011 = dot(gradient(c_011), unit_011);
-	float x111 = dot(gradient(c_111), unit_111);
-   
-    float y0 = mix(x000, x100, wx);
-    float y1 = mix(x001, x101, wx);
-    float y2 = mix(x010, x110, wx);
-    float y3 = mix(x011, x111, wx);
-    
-	float z0 = mix(y0, y2, wy);
-    float z1 = mix(y1, y3, wy);
-    
-    return mix(z0, z1, wz);
-}
-
 void main()
 {
     fs_Col = vs_Col;                         // Pass the vertex colors to the fragment shader for interpolation
@@ -143,9 +47,6 @@ void main()
                                                             // perpendicular to the surface after the surface is transformed by
                                                             // the model matrix.
                                                             
-    float freq = 7.0;  
-    float noiseVal = 1.0 - abs(noise(freq * vs_Pos.xyz));
-
     vec4 final_Pos = vs_Pos;
     final_Pos.y += 1.0 / 2.5 * sin(0.01 * u_Time) * fs_Nor.z;
     final_Pos.z += 1.0 / 2.5 * sin(0.01 * u_Time) * fs_Nor.y;
